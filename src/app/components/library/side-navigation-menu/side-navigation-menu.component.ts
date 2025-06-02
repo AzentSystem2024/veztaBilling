14cc5cc2c4f9fc3e65d12941c63fc66c6c13f4d3
sneha-sundaran03 <sneha@reanazentit.com>
2025-06-02 12:58:34 +0530

    changes

--- src/app/components/library/side-navigation-menu/side-navigation-menu.component.ts
+++ src/app/components/library/side-navigation-menu/side-navigation-menu.component.ts
@@ -175,7 +175,7 @@ refreshMenu() {
   // Settings (Admin only)
   if (isAdmin) {
     this.items.push({
-      text: 'settings',
+      text: 'Settings',
       icon: 'preferences',
       path: '/dashboard',
       items: [
@@ -228,7 +228,7 @@ refreshMenu() {
       { text: 'Date Wise Summary', path: '/datewise-summary' },
       { text: 'Bill Wise Summary', path: '/billwise-summary' },
       // { text: 'Staff Wise Summary', path: '/staffwise-summary' },
-      { text: 'Test Item Wise Summary', path: '/testitemwise-summary' },
+      { text: 'Item Wise Summary', path: '/testitemwise-summary' },
       { text: 'Scheme Wise Summary', path: '/schemewise-summary' },
       // { text: 'Scheme Wise Bill Summary', path: '/schemewise-bill-summary' },
       // { text: 'Patient Wise Summary', path: '/patientwise-summary' },
